8aa33a912f51d554f76f08c6858406ba4ebe232a
randombiker <adrian.benea@gmail.com>
2020-01-16 21:36:06 -0500

    First commit

---
+++ freecontactformsettings.php
@@ -0,0 +1,12 @@
+<?php
+$email_to = "simply_ady@yahoo.com"; // your email address
+$email_subject = "Contact Form Message"; // email subject line
+$thankyou = "thankyou.htm"; // thank you page
+
+// if you update the question on the form -
+// you need to update the questions answer below
+$antispam_answer = "25";
+
+// Do not change this line
+$base = "CgpGb3JtIHBvd2VyZWQgYnkgaHR0cHM6Ly93d3cuZnJlZWNvbnRhY3Rmb3JtLmNvbQ==";
+?>
